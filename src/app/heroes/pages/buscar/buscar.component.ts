775201fef05c4dcd118fbaf13aa51d5171f9393c
Javier <javier.sanchez28@protonmail.com>
2023-03-07 23:36:24 -0600

    Obs agregado a la busqueda

--- src/app/heroes/pages/buscar/buscar.component.ts
+++ src/app/heroes/pages/buscar/buscar.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { MatAutocompleteActivatedEvent } from '@angular/material/autocomplete';
 import { Heroe } from '../../interfaces/heroe.interface';
 import { HeroesService } from '../../services/heroes.service';
+import { Observable } from 'rxjs';
+import { ProgressBarService } from '../../../services/progress-bar.service';
 
 @Component({
   selector: 'app-buscar',
@@ -10,23 +12,23 @@ import { HeroesService } from '../../services/heroes.service';
 })
 export class BuscarComponent implements OnInit {
 
-  termino:string = '';
-  heroes: Heroe[] = [];
+  termino: string = '';
+  heroesObs$ = new Observable<Heroe[]>;
   heroeSeleccionado!: Heroe | undefined;
 
-  constructor(private heroesService: HeroesService) { }
+  constructor(private heroesService: HeroesService,
+    private progressBarService: ProgressBarService) { }
 
   ngOnInit(): void {
   }
 
-  buscando(): void{
-    this.heroesService.getHeroeSugerencias(this.termino)
-      .subscribe( heroes => this.heroes = heroes);
+  buscando(): void {
+    this.heroesObs$ = this.heroesService.getHeroeSugerencias(this.termino)
   }
 
-  opcionSeleccionada( event: MatAutocompleteActivatedEvent) {
+  opcionSeleccionada(event: MatAutocompleteActivatedEvent) {
 
-    if(!event.option?.value){
+    if (!event.option?.value) {
       this.heroeSeleccionado = undefined;
       return;
     }
@@ -34,7 +36,7 @@ export class BuscarComponent implements OnInit {
     const heroe: Heroe = event.option?.value;
     this.termino = heroe.superhero;
 
-    this.heroesService.getHeroe(heroe.id! )
-      .subscribe( heroe => this.heroeSeleccionado = heroe);
+    this.heroesService.getHeroe(heroe.id!)
+      .subscribe(heroe => this.heroeSeleccionado = heroe);
   }
 }
